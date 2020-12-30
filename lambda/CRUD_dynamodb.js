527c293e43e57973ac2bd2c6ce92329aed66943f
Pranay-Narang <pranaynarang@gmail.com>
2020-12-30 19:09:50 +0530

    lambda: CRUD app: Add support for PUT and DELETE

--- lambda/CRUD_dynamodb.js
+++ lambda/CRUD_dynamodb.js
@@ -38,5 +38,42 @@ exports.handler = async (event) => {
             body: "Item Added Successfully"
         }
         return response;
+    } else if (requestMethod == "PUT") {
+        var { id, name, age } = JSON.parse(event.body)
+
+        const params = {
+            TableName: tableName,
+            Item: {
+                id: id,
+                name: name,
+                age: age
+            }
+        };
+
+        const data = await documentClient.put(params).promise();
+
+        const response = {
+            statusCode: 200,
+            body: "Update Successful"
+        }
+        return response;
+    } else if (requestMethod == "DELETE") {
+        var { id } = JSON.parse(event.body);
+
+        const params = {
+            TableName: tableName,
+            Key: {
+                id: id,
+            }
+        }
+
+        const data = await documentClient.delete(params).promise();
+
+        const response = {
+            statusCode: 200,
+            body: "Delete Successful"
+        }
+
+        return response;
     }
 };
