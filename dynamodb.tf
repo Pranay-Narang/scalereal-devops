0b9879df795da9a5fb6efd4020007f18894d14e8
Pranay-Narang <pranaynarang@gmail.com>
2020-12-30 22:51:41 +0530

    dynamodb: Change primary attribute type to String

--- dynamodb.tf
+++ dynamodb.tf
@@ -2,9 +2,9 @@ resource "aws_dynamodb_table" "CSVWrite" {
     name = "CSVWrite"
     billing_mode = "PAY_PER_REQUEST"
     hash_key = "id"
-    
+
     attribute {
         name = "id"
-        type = "N"
+        type = "S"
     }
 }
