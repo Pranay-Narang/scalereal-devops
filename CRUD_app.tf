a6a523e8b3ebe67bb96954ce066b9fe0b7bb5365
Pranay-Narang <pranaynarang@gmail.com>
2020-12-31 15:53:50 +0530

    terraform: Create lambda function for CRUD app
    Extension of
    https://github.com/Pranay-Narang/scalereal-devops/commit/5c69411ee6d32f447d29611f9eb575354afbfce7

---
+++ CRUD_app.tf
@@ -0,0 +1,49 @@
+resource "aws_iam_role" "lambda_CRUD_execution_role" {
+  name = "lambda_CRUD_execution_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "dynamodb_access_policy" {
+    name = "dynamodb_access_policy"
+    policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "dynamodb:*",
+            "Effect": "Allow",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "dynamodb_access_policy_attachment" {
+    roles = [ aws_iam_role.lambda_CRUD_execution_role.name ]
+    policy_arn = aws_iam_policy.dynamodb_access_policy.arn
+
+    name = "dynamodb_policy_attachment"
+}
+
+resource "aws_lambda_function" "CRUD_app" {
+  function_name = "CRUD_app"
+  filename      = "lambda/CRUD-app.zip"
+  role          = aws_iam_role.lambda_CRUD_execution_role.arn
+  handler       = "CRUD_dynamodb.handler"
+  runtime       = "nodejs12.x"
+}
