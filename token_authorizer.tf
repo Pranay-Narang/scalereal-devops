5df7a54110783e76a8ec29541e65f6ac6edf3c31
Pranay-Narang <pranaynarang@gmail.com>
2020-12-31 19:14:35 +0530

    apigw: Attach token authorizer to gateway
    - Creates the lambda authorizer in API gateway

--- token_authorizer.tf
+++ token_authorizer.tf
@@ -24,3 +24,50 @@ resource "aws_lambda_function" "token_authorizer" {
   handler       = "token_authorizer.handler"
   runtime       = "nodejs12.x"
 }
+
+resource "aws_api_gateway_authorizer" "lambda_auth" {
+  name                   = "lambda_auth"
+  rest_api_id            = aws_api_gateway_rest_api.CRUD_gateway.id
+  authorizer_uri         = aws_lambda_function.token_authorizer.invoke_arn
+  authorizer_credentials = aws_iam_role.gateway_invocation_role.arn
+  identity_source = "method.request.header.authorizationToken"
+}
+
+resource "aws_iam_role" "gateway_invocation_role" {
+  name = "gateway_invocation_role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "gateway_invocation_policy" {
+  name = "gateway_invocation_policy"
+  role = aws_iam_role.gateway_invocation_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "lambda:InvokeFunction",
+      "Effect": "Allow",
+      "Resource": "${aws_lambda_function.token_authorizer.arn}"
+    }
+  ]
+}
+EOF
+}
